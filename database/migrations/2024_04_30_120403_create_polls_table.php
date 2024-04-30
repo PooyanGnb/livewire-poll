15a1f3a298ce8fe433ae81425423fe2e4b0471a3
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-04-30 16:27:11 +0330

    migration and tables

--- database/migrations/2024_04_30_120403_create_polls_table.php
+++ database/migrations/2024_04_30_120403_create_polls_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('polls', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+
             $table->timestamps();
         });
     }
